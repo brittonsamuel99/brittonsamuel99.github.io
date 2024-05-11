1af7e0880d2585e4fd801d03531f57d327ab887a
Sam Britton <48329862+brittonsamuel99@users.noreply.github.com>
2024-05-11 15:53:58 -0600

    Verify actions

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 export default function Home() {
   return (
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello World</title>
-        <style>
-            body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-                font-family: Arial, sans-serif;
-                font-size: 24px;
-                background-color: #f0f0f0; /* Light gray background */
-            }
-        </style>
-    </head>
-    <body>
-        Hello world
-    </body>
+    <div>
+      <div className="grid h-screen place-items-center bg-purple-500">
+        <div>
+          \/\/ TODO: Make a website
+        </div>
+      </div>
+
+      <div className="circles">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   );
 }
